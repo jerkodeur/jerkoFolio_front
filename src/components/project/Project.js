2b641edf3fbd2cb0c9b50fd0738c6cd2e8acc6cb
JerkoMintHome <jerome.potie@gmail.com>
2020-07-26 17:35:25 +0200

    End of the project display page

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -10,6 +10,10 @@ import Header from '../commons/Header'
 
 import './Project.css'
 
+import Edit from '../../images/modify.png'
+import Www from '../../images/www.svg'
+import Github from '../../images/github.png'
+
 const url = process.env.REACT_APP_API_URL
 
 const Project = (props) => {
@@ -47,22 +51,82 @@ const Project = (props) => {
             const side = id % 2 === 0 ? 'left' : 'right'
             return (
               <div className='flex-gen-project' key={id}>
-                {side === 'right' && windowSize > 1150 && <div className='description'><ReactMarkdown source={description} /></div>}
-                  {side === 'right' && windowSize > 1150 && <DisplayTechnos listTechnos={technos} size={windowSize} />}
+                {side === 'right' && windowSize > 1150 &&
+                  <>
+                    <div className='cont-description'>
+                      <div className='description'>
+                        <ReactMarkdown source={description} />
+                      </div>
+                      <div className='flex-github'>
+                        <img src={Github} alt='' />
+                        <a href={url_github} title='Voir le dépôt github du projet' target='_blank' rel="noopener noreferrer">
+                          {url_github}
+                        </a>
+                      </div>
+                    </div>
+                    <DisplayTechnos listTechnos={technos} />
+                  </>
+                }
                 <div className={side}>
-                  <div className='title'>
-                    {side === 'left' && <h1>{title}</h1>}
-                    <span className={side === 'left' ? 'date-right' : 'date-left'}>{tempDate}</span>
-                    {side === 'right' && <h1>{title}</h1>}
+                  <div className='infos-project'>
+                    {side === 'left' &&
+                      <a href='#' title='Modifier le contenu du projet'>Modifier</a>
+                    }
+                    <div className='title'>
+                      {side === 'left' &&
+                        <>
+                          <h1>{title}</h1>
+                        </>
+                      }
+                      <span className={side === 'left' ? 'date-right' : 'date-left'}>
+                        {side === 'left' &&
+                          <>
+                            <a href={url_test} title='Voir le site' target='_blank' rel="noopener noreferrer">
+                              <img src={Www} alt='' />
+                            </a>
+                          </>
+                        }
+                        {tempDate}
+
+                        {side === 'right' &&
+                          <>
+                          <a href={url_test} title='Voir le site' target='_blank' rel="noopener noreferrer">
+                            <img src={Www} alt='' />
+                          </a>
+                          </>
+                          }
+                      </span>
+                      {side === 'right' && <h1>{title}</h1>}
+                    </div>
+                          {side === 'right' &&
+                            <a href='#' title='Modifier le contenu du projet'>Modifier</a>
+                          }
                   </div>
                   <div className={side === 'left' ? 'cont-screenshot-Tleft' : 'cont-screenshot-Tright'}>
-                    {side === 'right' && windowSize < 1150 && <DisplayTechnos listTechnos={technos} size={windowSize} side='right' />}
+                    {side === 'right' && windowSize < 1150 &&
+                      <DisplayTechnos listTechnos={technos} />
+                    }
                     <img src={tempImage} alt='Screenshot du projet' className='project-img' />
-                    {side === 'left' && windowSize < 1150 && <DisplayTechnos listTechnos={technos} size={windowSize} side='left' />}
+                    {side === 'left' && windowSize < 1150 &&
+                      <DisplayTechnos listTechnos={technos} />
+                    }
                   </div>
                 </div>
-                {side === 'left' && windowSize > 1150 && <DisplayTechnos listTechnos={technos} size={windowSize} />}
-                {(side === 'left' || windowSize < 1150) && <div className='description'><ReactMarkdown source={description} /></div>}
+                {side === 'left' && windowSize > 1150 &&
+                  <DisplayTechnos listTechnos={technos} />}
+                {(side === 'left' || windowSize < 1150) &&
+                  <div className='cont-description'>
+                    <div className='description'>
+                      <ReactMarkdown source={description} />
+                    </div>
+                    <div className='flex-github'>
+                      <img src={Github} alt='' />
+                      <a href={url_github} title='Voir le dépôt github du projet' target='_blank' rel="noopener noreferrer">
+                        {url_github}
+                      </a>
+                    </div>
+                  </div>
+                }
               </div>
             )
           })
